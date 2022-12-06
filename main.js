7f7124ac6f2cd79e3c78937c012a7bc313181b52
NyazyDev <77538620+NyazyDev@users.noreply.github.com>
2022-12-06 22:41:38 +0200

    Some updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function vajutus() {
-    var x = document.getElementById("av");
+    var x = document.getElementById("nav");
     if (x.className === "class") {
       x.className += " responsive";
     } else {
